d2bf396ba4aaf53645129e1edb00488c4d78c936
ibufu <chenxuanincn@gmail.com>
2023-06-09 16:33:21 +0400

    feat: cat miao

--- src/List.tsx
+++ src/List.tsx
@@ -23,7 +23,7 @@ function List({ api }: { api: () => Promise<any> }) {
   return (
     <div>
       <p>
-        <img width={300} src={data?.url} />
+        <img width={300} src={data?.url} alt="cat" />
       </p>
       <p>id: {data?.id}</p>
       <p>url: {data?.url}</p>
