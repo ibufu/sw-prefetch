420233a2e89cb3da4a9059a9a3f4e4cddabd4551
ibufu <chenxuanincn@gmail.com>
2023-06-09 16:45:20 +0400

    feat: change layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,14 @@ import { getData1, getData2 } from "./api";
 function App() {
   return (
     <div className="App">
-      <h3>Without Prefetch</h3>
-      <List api={getData2} />
-      <h3>Prefetch</h3>
-      <List api={getData1} />
+      <div>
+        <h3>Without Prefetch</h3>
+        <List api={getData2} />
+      </div>
+      <div>
+        <h3>Prefetch</h3>
+        <List api={getData1} />
+      </div>
     </div>
   );
 }
